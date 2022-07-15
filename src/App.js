07a932a6caf5b71793124e6a9c47a98e7f096711
RuntimeTerror10 <parthb.git@gmail.com>
2022-07-16 00:38:37 +0530

    parse the pasted url and render the url params

--- src/App.js
+++ src/App.js
@@ -3,16 +3,37 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [url, setUrl] = useState("");
+  const [parsed, setParsed] = useState({});
+  console.log("parsed", parsed);
+  // const [isUrlLoading, setIsUrlLoading] = useState(false);
+  const httpRegex = "/^www./";
+
+  const checkHttp = (url) => {
+    if (url.includes("http://") || url.includes("https://www.")) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   const copyEvent = (event) => {
     event.preventDefault();
     let charCode = String.fromCharCode(event.which).toLocaleLowerCase();
     if ((event.ctrlKey || event.metaKey) && charCode === "v") {
       navigator.clipboard.readText().then((text) => {
-        if (text.includes("https://") || text.includes("http://")) {
-          setUrl(text);
+        if (text.length > 1) {
+          let httpTest = httpRegex.match(text);
+          if (checkHttp(text)) {
+            setUrl(text);
+          } else if (httpTest) {
+            setUrl(`https://${text}`);
+          } else {
+            alert(
+              "Please paste an url like \n https://www.example.com or http://ex.com or www.ex.com"
+            );
+          }
         } else {
-          alert("Please copy a valid URL");
+          alert("Please copy an URL first");
         }
       });
     }
@@ -25,10 +46,58 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    if (url.startsWith("http")) {
+      let tempObj = new URL(url);
+      setParsed({
+        origin: tempObj.origin,
+        protocol: tempObj.protocol,
+        username: tempObj.username,
+        password: tempObj.password,
+        host: tempObj.host,
+        hostname: tempObj.hostname,
+        port: tempObj.port,
+        pathname: tempObj.pathname,
+        search: tempObj.search,
+        hash: tempObj.hash,
+        searchParams: tempObj.searchParams,
+      });
+    }
+  }, [url]);
+
   return (
     <div className="app-container">
-      {url.length ? (
-        <div className="url">{url}</div>
+      {url.includes("http") ? (
+        <div className="result">
+          <div className="pasted-url">{url}</div>
+          <div className="parsed-container">
+            <div className="param-container">
+              <div>{parsed.protocol}</div>
+              <div className="param-name">protocol</div>
+            </div>
+            <div className="defaults">://</div>
+            <div className="param-container">
+              <div>{parsed.hostname}</div>
+              <div className="param-name">hostname</div>
+            </div>
+            {parsed.port ? (
+              <div className="param-container">
+                <div>{parsed.port}</div>
+                <div className="param-name">port</div>
+              </div>
+            ) : null}
+            <div className="param-container">
+              <div>{parsed.pathname}</div>
+              <div className="param-name">pathname</div>
+            </div>
+            {parsed.search ? (
+              <div className="param-container">
+                <div>{parsed.search}</div>
+                <div className="param-name">Search</div>
+              </div>
+            ) : null}
+          </div>
+        </div>
       ) : (
         <div className="url-empty">
           Copy any URL on this window and see the magic!
