4bb375992891e3d2a95c271f0aabe63b46f3cb4b
RuntimeTerror10 <parthb.git@gmail.com>
2022-07-17 13:12:00 +0530

    map arrows from each url param to their respective param name

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { ArcherContainer, ArcherElement } from "react-archer";
 
 function App() {
   const [url, setUrl] = useState("");
@@ -16,7 +17,7 @@ function App() {
     }
   };
 
-  const copyEvent = (event) => {
+  const keyDownHandler = (event) => {
     let charCode = String.fromCharCode(event.which).toLocaleLowerCase();
     if ((event.ctrlKey || event.metaKey) && charCode === "v") {
       navigator.clipboard.readText().then((text) => {
@@ -39,9 +40,9 @@ function App() {
   };
 
   useEffect(() => {
-    window.addEventListener("keydown", copyEvent);
+    window.addEventListener("keydown", keyDownHandler);
     return () => {
-      window.removeEventListener("keydown", copyEvent);
+      window.removeEventListener("keydown", keyDownHandler);
     };
   }, []);
 
@@ -68,34 +69,160 @@ function App() {
     <div className="app-container">
       {url.includes("http") ? (
         <div className="result">
-          <div className="pasted-url">{url}</div>
-          <div className="parsed-container">
-            <div className="param-container">
-              <div>{parsed.protocol}</div>
-              <div className="param-name">protocol</div>
-            </div>
-            <div className="defaults">://</div>
-            <div className="param-container">
-              <div>{parsed.hostname}</div>
-              <div className="param-name">hostname</div>
-            </div>
-            {parsed.port ? (
-              <div className="param-container">
-                <div>{parsed.port}</div>
-                <div className="param-name">port</div>
+          <ArcherContainer strokeColor="black" style={{ width: "100%" }}>
+            <div className="pasted-url">
+              <div>
+                <ArcherElement
+                  id="protocol"
+                  relations={[
+                    {
+                      targetId: "protocol_box",
+                      targetAnchor: "top",
+                      sourceAnchor: "bottom",
+                      style: { strokeWidth: 1.5, lineStyle: "straight" },
+                    },
+                  ]}
+                >
+                  <div>{parsed.protocol}//</div>
+                </ArcherElement>
               </div>
-            ) : null}
-            <div className="param-container">
-              <div>{parsed.pathname}</div>
-              <div className="param-name">pathname</div>
-            </div>
-            {parsed.search ? (
-              <div className="param-container">
-                <div>{parsed.search}</div>
-                <div className="param-name">Search</div>
+              <div>
+                <ArcherElement
+                  id="hostname"
+                  relations={[
+                    {
+                      targetId: "hostname_box",
+                      targetAnchor: "top",
+                      sourceAnchor: "bottom",
+                      style: { strokeWidth: 1.5, lineStyle: "straight" },
+                    },
+                  ]}
+                >
+                  <div>{parsed.hostname}</div>
+                </ArcherElement>
               </div>
-            ) : null}
-          </div>
+              {parsed.port ? (
+                <div>
+                  <ArcherElement
+                    id="port"
+                    relations={[
+                      {
+                        targetId: "port_box",
+                        targetAnchor: "top",
+                        sourceAnchor: "bottom",
+                        style: { strokeWidth: 1.5, lineStyle: "straight" },
+                      },
+                    ]}
+                  >
+                    <div>{parsed.port}</div>
+                  </ArcherElement>
+                </div>
+              ) : null}
+              <div>
+                <ArcherElement
+                  id="pathname"
+                  relations={[
+                    {
+                      targetId: "pathname_box",
+                      targetAnchor: "top",
+                      sourceAnchor: "bottom",
+                      style: { strokeWidth: 1.5, lineStyle: "straight" },
+                    },
+                  ]}
+                >
+                  <div>{parsed.pathname}</div>
+                </ArcherElement>
+              </div>
+              {parsed.search ? (
+                <div>
+                  <ArcherElement
+                    id="search"
+                    relations={[
+                      {
+                        targetId: "search_box",
+                        targetAnchor: "top",
+                        sourceAnchor: "bottom",
+                        style: { strokeWidth: 1.5, lineStyle: "straight" },
+                      },
+                    ]}
+                  >
+                    <div>{parsed.search}</div>
+                  </ArcherElement>
+                </div>
+              ) : null}
+              {parsed.hash ? (
+                <div>
+                  <ArcherElement
+                    id="hash"
+                    relations={[
+                      {
+                        targetId: "hash_box",
+                        targetAnchor: "top",
+                        sourceAnchor: "bottom",
+                        style: { strokeWidth: 1.5, lineStyle: "straight" },
+                      },
+                    ]}
+                  >
+                    <div>{parsed.hash}</div>
+                  </ArcherElement>
+                </div>
+              ) : null}
+              <div>
+                <ArcherElement
+                  id="search"
+                  relations={[
+                    {
+                      targetId: "search_box",
+                      targetAnchor: "top",
+                      sourceAnchor: "bottom",
+                      style: { strokeWidth: 1.5 },
+                    },
+                  ]}
+                >
+                  <div>{parsed.search}</div>
+                </ArcherElement>
+              </div>
+            </div>
+            <div className="parsed-container">
+              <div className="param-container">
+                <ArcherElement id="protocol_box">
+                  <div className="param-name">protocol</div>
+                </ArcherElement>
+              </div>
+
+              <div className="param-container">
+                <ArcherElement id="hostname_box">
+                  <div className="param-name">hostname</div>
+                </ArcherElement>
+              </div>
+              {parsed.port ? (
+                <div className="param-container">
+                  <ArcherElement id="port_box">
+                    <div className="param-name">port</div>
+                  </ArcherElement>
+                </div>
+              ) : null}
+              <div className="param-container">
+                <ArcherElement id="pathname_box">
+                  <div className="param-name">pathname</div>
+                </ArcherElement>
+              </div>
+              {parsed.search ? (
+                <div className="param-container">
+                  <ArcherElement id="search_box">
+                    <div className="param-name">Search</div>
+                  </ArcherElement>
+                </div>
+              ) : null}
+              {parsed.hash ? (
+                <div className="param-container">
+                  <ArcherElement id="hash_box">
+                    <div className="param-name">hash</div>
+                  </ArcherElement>
+                </div>
+              ) : null}
+            </div>
+          </ArcherContainer>
         </div>
       ) : (
         <div className="url-empty">
