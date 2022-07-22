6eb956b5e0945bd394facf00c87be1c9b3c48a08
RuntimeTerror10 <parthb.git@gmail.com>
2022-07-23 00:23:15 +0530

    added pasted url separation and param name animation

--- src/Components/ParsedContainer.js
+++ src/Components/ParsedContainer.js
@@ -1,209 +1,240 @@
-import { ArcherContainer, ArcherElement } from "react-archer";
+// import { ArcherContainer, ArcherElement } from "react-archer";
 import { motion } from "framer-motion";
 
 export const ParsedContainer = ({ parsed }) => {
   return (
-    <div className="result">
-      <ArcherContainer strokeColor="black" style={{ width: "100%" }}>
-        <div className="pasted-url">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+      }}
+      className="result"
+    >
+      <motion.div
+        className="pasted-url"
+        initial={{ gap: 0 }}
+        animate={{ gap: "1rem" }}
+        transition={{ delay: 0.8 }}
+      >
+        <div className="param-group">
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            animate={{
+              padding: "1rem",
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+            }}
+            transition={{ delay: 1.4 }}
+            className="root-param"
           >
-            <ArcherElement
-              id="protocol"
-              relations={[
-                {
-                  targetId: "protocol_box",
-                  targetAnchor: "top",
-                  sourceAnchor: "bottom",
-                  style: { strokeWidth: 1.5, lineStyle: "straight" },
-                },
-              ]}
-            >
-              <div className="root-param">
-                <div>{parsed.protocol}//</div>
-              </div>
-            </ArcherElement>
+            {parsed.protocol}
           </motion.div>
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
+            initial={{ width: 0, opacity: 0, y: -20 }}
+            animate={{
+              width: "auto",
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ delay: 2 }}
+            className="param-name"
           >
-            <ArcherElement
-              id="hostname"
-              relations={[
-                {
-                  targetId: "hostname_box",
-                  targetAnchor: "top",
-                  sourceAnchor: "bottom",
-                  style: { strokeWidth: 1.5, lineStyle: "straight" },
-                },
-              ]}
-            >
-              <div className="root-param">
-                <div>{parsed.hostname}</div>
-              </div>
-            </ArcherElement>
+            protocol
           </motion.div>
-          {parsed.port ? (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-            >
-              <ArcherElement
-                id="port"
-                relations={[
-                  {
-                    targetId: "port_box",
-                    targetAnchor: "top",
-                    sourceAnchor: "bottom",
-                    style: { strokeWidth: 1.5, lineStyle: "straight" },
-                  },
-                ]}
-              >
-                <div className="root-param">
-                  <div>{parsed.port}</div>
-                </div>
-              </ArcherElement>
-            </motion.div>
-          ) : null}
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5 }}
-          >
-            <ArcherElement
-              id="pathname"
-              relations={[
-                {
-                  targetId: "pathname_box",
-                  targetAnchor: "top",
-                  sourceAnchor: "bottom",
-                  style: { strokeWidth: 1.5, lineStyle: "straight" },
-                },
-              ]}
-            >
-              <div className="root-param">
-                <div>{parsed.pathname}</div>
-              </div>
-            </ArcherElement>
-          </motion.div>
-
-          {parsed.search ? (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-            >
-              <ArcherElement
-                id="search"
-                relations={[
-                  {
-                    targetId: "search_box",
-                    targetAnchor: "top",
-                    sourceAnchor: "bottom",
-                    style: { strokeWidth: 1.5, lineStyle: "straight" },
-                  },
-                ]}
-              >
-                <div className="root-param">
-                  <div>{parsed.search}</div>
-                </div>
-              </ArcherElement>
-            </motion.div>
-          ) : null}
-          {parsed.hash ? (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-            >
-              <ArcherElement
-                id="hash"
-                relations={[
-                  {
-                    targetId: "hash_box",
-                    targetAnchor: "top",
-                    sourceAnchor: "bottom",
-                    style: { strokeWidth: 1.5, lineStyle: "straight" },
-                  },
-                ]}
-              >
-                <div className="root-param">
-                  <div>{parsed.hash}</div>
-                </div>
-              </ArcherElement>
-            </motion.div>
-          ) : null}
-          {parsed.search ? (
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5 }}
-            >
-              <ArcherElement
-                id="search"
-                relations={[
-                  {
-                    targetId: "search_box",
-                    targetAnchor: "top",
-                    sourceAnchor: "bottom",
-                    style: { strokeWidth: 1.5 },
-                  },
-                ]}
-              >
-                <div className="root-param">
-                  <div>{parsed.search}</div>
-                </div>
-              </ArcherElement>
-            </motion.div>
-          ) : null}
         </div>
-        <div className="parsed-container">
-          <ArcherElement id="protocol_box">
-            <div className="param-container">
-              <div className="param-name">protocol</div>
-            </div>
-          </ArcherElement>
-
-          <ArcherElement id="hostname_box">
-            <div className="param-container">
-              <div className="param-name">hostname</div>
-            </div>
-          </ArcherElement>
-          {parsed.port ? (
-            <ArcherElement id="port_box">
-              <div className="param-container">
-                <div className="param-name">port</div>
-              </div>
-            </ArcherElement>
-          ) : null}
-          <ArcherElement id="pathname_box">
-            <div className="param-container">
-              <div className="param-name">pathname</div>
-            </div>
-          </ArcherElement>
-          {parsed.search ? (
-            <ArcherElement id="search_box">
-              <div className="param-container">
-                <div className="param-name">Search</div>
-              </div>
-            </ArcherElement>
-          ) : null}
-          {parsed.hash ? (
-            <ArcherElement id="hash_box">
-              <div className="param-container">
-                <div className="param-name">hash</div>
-              </div>
-            </ArcherElement>
-          ) : null}
+        <div className="param-group">
+          <motion.div
+            animate={{
+              padding: "1rem",
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+            }}
+            transition={{ delay: 1.4 }}
+            className="root-param"
+          >
+            <div>{parsed.hostname}</div>
+          </motion.div>
+          <motion.div
+            initial={{ width: 0, opacity: 0, y: -20 }}
+            animate={{
+              width: "auto",
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ delay: 2.2 }}
+            className="param-name"
+          >
+            hostname
+          </motion.div>
         </div>
-      </ArcherContainer>
-    </div>
+        {parsed.port ? (
+          <div className="param-group">
+            <motion.div
+              animate={{
+                padding: "1rem",
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+              }}
+              transition={{ delay: 1.4 }}
+              className="root-param"
+            >
+              <div>{parsed.port}</div>
+            </motion.div>
+            <motion.div
+              initial={{ width: 0, opacity: 0, y: -20 }}
+              animate={{
+                width: "auto",
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{ delay: 2.4 }}
+              className="param-name"
+            >
+              port
+            </motion.div>
+          </div>
+        ) : null}
+        <div className="param-group">
+          <motion.div
+            animate={{
+              padding: "1rem",
+              boxShadow:
+                "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+            }}
+            transition={{ delay: 1.4 }}
+            className="root-param"
+          >
+            <div>{parsed.pathname}</div>
+          </motion.div>
+          <motion.div
+            initial={{ width: 0, opacity: 0, y: -20 }}
+            animate={{
+              width: "auto",
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{ delay: parsed.port ? 2.6 : 2.4 }}
+            className="param-name"
+          >
+            pathname
+          </motion.div>
+        </div>
+
+        {parsed.search ? (
+          <div className="param-group">
+            <motion.div
+              animate={{
+                padding: "1rem",
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+              }}
+              transition={{ delay: 1.4 }}
+              className="root-param"
+            >
+              <div>{parsed.search}</div>
+            </motion.div>
+            <motion.div
+              initial={{ width: 0, opacity: 0, y: -20 }}
+              animate={{
+                width: "auto",
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{ delay: parsed.port ? 2.8 : 2.6 }}
+              className="param-name"
+            >
+              search
+            </motion.div>
+          </div>
+        ) : null}
+        {parsed.hash ? (
+          <div className="param-group">
+            <motion.div
+              animate={{
+                padding: "1rem",
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+              }}
+              transition={{ delay: 1.4 }}
+              className="root-param"
+            >
+              <div>{parsed.hash}</div>
+            </motion.div>
+            <motion.div
+              initial={{ width: 0, opacity: 0, y: -20 }}
+              animate={{
+                width: "auto",
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{
+                delay: parsed.port
+                  ? 3
+                  : parsed.search && !parsed.port
+                  ? 2.8
+                  : 3.2,
+              }}
+              className="param-name"
+            >
+              hash
+            </motion.div>
+          </div>
+        ) : null}
+        {/* {parsed.search ? (
+          <div>
+            <motion.div
+              animate={{
+                padding: "1rem",
+                boxShadow:
+                  "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+              }}
+              transition={{ delay: 800 1.4 }}
+              className="root-param"
+            >
+              <div>{parsed.search}</div>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: -20 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{ delay: 2 }}
+            >
+              search
+            </motion.div>
+          </div>
+        ) : null} */}
+      </motion.div>
+      {/* <motion.div className="parsed-container">
+        <div className="param-container">
+          <div className="param-name">protocol</div>
+        </div>
+
+        <div className="param-container">
+          <div className="param-name">hostname</div>
+        </div>
+
+        {parsed.port ? (
+          <div className="param-container">
+            <div className="param-name">port</div>
+          </div>
+        ) : null}
+
+        <div className="param-container">
+          <div className="param-name">pathname</div>
+        </div>
+
+        {parsed.search ? (
+          <div className="param-container">
+            <div className="param-name">Search</div>
+          </div>
+        ) : null}
+        {parsed.hash ? (
+          <div className="param-container">
+            <div className="param-name">hash</div>
+          </div>
+        ) : null}
+      </motion.div> */}
+    </motion.div>
   );
 };
