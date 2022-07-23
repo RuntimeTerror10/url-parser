2471e61cc7a0f13a4df4632ff09f0ffa77f14460
RuntimeTerror10 <parthb.git@gmail.com>
2022-07-23 19:41:21 +0530

    minor changes

--- src/Components/ParsedContainer.js
+++ src/Components/ParsedContainer.js
@@ -2,9 +2,17 @@
 import { motion } from "framer-motion";
 
 export const ParsedContainer = ({ parsed }) => {
-  const parameters = ["hostname", "port", "pathname", "search", "hash"];
+  const parameters = [
+    "protocol",
+    "hostname",
+    "port",
+    "pathname",
+    "search",
+    "hash",
+  ];
   let k = 0,
     init = 2;
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -19,40 +27,15 @@ export const ParsedContainer = ({ parsed }) => {
         animate={{ gap: "2rem" }}
         transition={{ delay: 0.8 }}
       >
-        <div className="param-group">
-          <motion.div
-            animate={{
-              padding: "1rem",
-              boxShadow:
-                "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
-              borderRadius: "5px",
-            }}
-            transition={{ delay: 1.4 }}
-            className="root-param"
-          >
-            {parsed.protocol}//
-          </motion.div>
-          <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{
-              opacity: 1,
-              y: 0,
-            }}
-            transition={{ delay: 2 }}
-            className="param-name"
-          >
-            protocol
-          </motion.div>
-        </div>
         {parameters.map((param, index) => {
           parsed[`${param}`] ? (k += 0.1) : (k = k);
+
           return parsed[`${param}`] ? (
-            <div className="param-group" key={index}>
+            <div className={`param-group param-${param}`} key={index}>
               <motion.div
                 animate={{
                   padding: "1rem",
-                  boxShadow:
-                    "rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px",
+                  boxShadow: "rgb(0 0 0 / 20%) 0px 0px 0px 3px",
                   borderRadius: "5px",
                 }}
                 transition={{ delay: 1.4 }}
